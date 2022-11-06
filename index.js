44037f126c3f7488b5ead387c05891a1472baed1
ozzychel <andrey.petunin86@gmail.com>
2022-11-05 17:51:26 -0700

    add public folder and serve static

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 3080;
 const bodyParser = require('body-parser');
+const path = require('path');
 const morgan = require('morgan');
 
 app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+app.use(express.static(path.join(__dirname, './public')));
 app.get('/', (req, res) => {
   console.log(">>REQ_QUERY:", req.query)
   console.log(">>REQ_BODY:", req.body)
